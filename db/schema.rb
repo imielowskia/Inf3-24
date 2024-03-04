a45e090b2ee1e3ba22639de0f0e8b307c3f20b46
imielowskia <andrzej.imielowski@pwste.edu.pl>
2024-03-04 14:25:32 +0100

    dodanie scaffold dla Field i poprawki w widokach

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_01_075005) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_04_131903) do
+  create_table "fields", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "shortname"
