bd172a19de25072dcb34d0d29f651105300d34a0
imielowskia <andrzej.imielowski@pwste.edu.pl>
2024-03-16 09:38:34 +0100

    oceny v.2 z turbo

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,4 +1,5 @@
 class Grade < ApplicationRecord
+  validates :grade, numericality: { greater_than_or_equal_to: 2, less_than_or_equal_to: 5 }
   belongs_to :student
   belongs_to :course
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
+  validates :indeks, format: { with: /\A\d{6}\z/, message: "tylko 6 cyfr"}
+
   belongs_to :group
   has_many :grades
   has_many :courses, through: :grades
